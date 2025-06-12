5c4e2db6358f0315691f3e70ecbb6b1fb9e957dc
Hmmam <bhmmam@gmail.com>
2025-06-12 19:37:17 +0300

    just setting up the files

--- watchingList/src/components/ListItem.jsx
+++ watchingList/src/components/ListItem.jsx
@@ -1,7 +1,10 @@
 const ListItem = ()=>{
     return(
         <div className="item">
-
+            <div className="date">
+                <div className="day"></div>
+                <div className="month-year"></div>
+            </div>
         </div>
     )
 }
