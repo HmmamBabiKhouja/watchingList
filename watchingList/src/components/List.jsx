379fcb223a662d940f4eae7a7a829774793f19f5
Hmmam <bhmmam@gmail.com>
2025-06-15 20:28:52 +0300

    Adds initial item list for upcoming shows
    Introduces a predefined list of upcoming shows with details including date, title, and description.
    
    This improvement enhances the component's functionality by providing data for rendering a list of shows, allowing for future display and management of viewing schedules.

--- watchingList/src/components/List.jsx
+++ watchingList/src/components/List.jsx
@@ -1,5 +1,20 @@
 import ListItem from "./ListItem";
 
+const items=[
+    { day: "27", month: "June", year:"2025", title: "Squid game", desc: "Squid game season 3" },
+    { day: "10", month: "July", year:"2025", title: "Dr Stone", desc: "the rest of Dr.stone season 4" },
+    { day: "19", month: "July", year:"2025", title: "Kaiju 8", desc: "Kaiju 8 season 2" },
+    { day: "14", month: "July", year:"2025", title: "Dan da dan", desc: "Dan da dan season 2" },
+    { day: "1", month: "Oct", year:"2025", title: "My hero academia", desc: "Boku no Hero Academia season 8" },
+    { day: "1", month: "Sept", year:"2025", title: "Alice in borderland", desc: "Alice in borderland season 3" },
+    { day: "11", month: "July", year:"2025", title: "Dexter", desc: "this season is named Resurrection" },
+    { day: "-", month: "-", year:"-", title: "House of David", desc: "series about the life of prophet David" },
+    { day: "-", month: "-", year:"-", title: "Silo", desc: "Set in a dystopian future where a community exists in a giant underground silo comprising 144 levels" },
+    { day: "-", month: "-", year:"-", title: "Dark matter", desc: "A physicist in Chicago is warped into an alternate version of his life, leaving him to fight to return to his life to prevent the alternate version of himself from taking over his family." },
+    { day: "-", month: "-", year:"-", title: "Dexter", desc: "this season is named Original sin" },
+
+]
+
 const List = () => {
     return (
         <div className="container">
