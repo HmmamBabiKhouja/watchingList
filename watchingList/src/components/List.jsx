7157753b5bbdc9ae268f2c78240c85d1bc42927a
Hmmam <bhmmam@gmail.com>
2025-06-16 17:51:29 +0300

    Refactors date handling and enhances styling in list component
    Improves date formatting by simplifying month indexing and updating item entries for consistency.
    
    Enhances CSS for better layout and visual presentation, ensuring a responsive design that adapts to various screen sizes.
    
    Adds new styles for date display elements, improving readability and user experience.

--- watchingList/src/components/List.jsx
+++ watchingList/src/components/List.jsx
@@ -1,27 +1,36 @@
 import ListItem from "./ListItem";
 
 const items=[
-    { day: "27", month: "June", year:"2025", title: "Squid game", desc: "Squid game season 3", cate: "Thriler"},
-    { day: "10", month: "July", year:"2025", title: "Dr Stone", desc: "the rest of Dr.stone season 4", cate: "Anime, Adventure"},
-    { day: "19", month: "July", year:"2025", title: "Kaiju 8", desc: "Kaiju 8 season 2", cate: "Anime, Shonen"},
-    { day: "14", month: "July", year:"2025", title: "Dan da dan", desc: "Dan da dan season 2", cate: "Anime, Shonen"},
+    { day: "27", month: "Jun", year:"2025", title: "Squid game", desc: "Squid game season 3", cate: "Thriler"},
+    { day: "10", month: "Jul", year:"2025", title: "Dr Stone", desc: "the rest of Dr.stone season 4", cate: "Anime, Adventure"},
+    { day: "19", month: "Jul", year:"2025", title: "Kaiju 8", desc: "Kaiju 8 season 2", cate: "Anime, Shonen"},
+    { day: "14", month: "Jul", year:"2025", title: "Dan da dan", desc: "Dan da dan season 2", cate: "Anime, Shonen"},
     { day: "01", month: "Oct", year:"2025", title: "My hero academia", desc: "Boku no Hero Academia season 8", cate: "Anime, Shonen" },
-    { day: "01", month: "Sept", year:"2025", title: "Alice in borderland", desc: "Alice in borderland season 3", cate: "Thriler"},
-    { day: "11", month: "July", year:"2025", title: "Dexter", desc: "this season is named Resurrection", cate: "Drama, Crime" },
-    { day: "__", month: "__", year:"__", title: "House of David", desc: "series about the life of prophet David", cate:"Historical drama"},
-    { day: "__", month: "__", year:"__", title: "Silo", desc: "Set in a dystopian future where a community exists in a giant underground silo comprising 144 levels", cate:"Drama" },
-    { day: "__", month: "__", year:"__", title: "Dark matter", desc: "A physicist in Chicago is warped into an alternate version of his life, leaving him to fight to return to his life to prevent the alternate version of himself from taking over his family.", cate:"Thriller"},
-    { day: "__", month: "__", year:"__", title: "Dexter", desc: "this season is named Original sin", cate:"Drama, Crime" },
+    { day: "01", month: "Sep", year:"2025", title: "Alice in borderland", desc: "Alice in borderland season 3", cate: "Thriler"},
+    { day: "11", month: "Jul", year:"2025", title: "Dexter", desc: "this season is named Resurrection", cate: "Drama, Crime" },
+    { day: "27", month: "Feb", year:"2025", title: "House of David", desc: "series about the life of prophet David", cate:"Historical drama"},
+    { day: "15", month: "Nov", year:"2024", title: "Silo", desc: "Silo season 2", cate:"Drama" },
+    { day: "08", month: "May", year:"2024", title: "Dark matter", desc: "A physicist is warped into an alternate version of his life, leaving him to fight to return to his life.", cate:"Thriller"},
+    { day: "13", month: "Dec", year:"2024", title: "Dexter", desc: "this season is named Original sin", cate:"Drama, Crime" },
 ]
 
 const getMonthIndex=(month)=>{
-    const months = [
-    'january', 'february', 'march', 'april', 'may', 'june',
-    'july', 'august', 'september', 'october', 'november', 'december'
-    ];
+const monthes = {
+    Jan: 1,
+    Feb: 2,
+    Mar: 3,
+    Apr: 4,
+    May: 5,
+    Jun: 6,
+    Jul: 7,
+    Aug: 8,
+    Sep: 9,
+    Oct: 10,
+    Nov: 11,
+    Dec: 12
+};
 
-    const lowerCaseMonthName = month.toLowerCase();
-    let index = months.indexOf(lowerCaseMonthName)+1;
+    let index = monthes[month]
     return index>=10? index : "0"+index;
 }
 
@@ -31,7 +40,8 @@ const remainDays = (date)=>{
 
     const differenceInTime = givenDate.getTime()-today.getTime()
     const differenceInDays = Math.ceil(differenceInTime/(1000*60*60*24))
-    return differenceInDays
+    
+    return differenceInDays<0? "already streamd" : differenceInDays
 }
 
 const List = () => {
