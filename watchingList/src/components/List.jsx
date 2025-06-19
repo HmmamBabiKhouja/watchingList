fe9cd5ae608a4cceb7ef7731dbb4a4b7c2839788
Hmmam <bhmmam@gmail.com>
2025-06-19 18:29:30 +0300

    Removes hardcoded movie data and imports initial shows
    Eliminates the hardcoded movie list from the component,
    replacing it with an imported initial list of shows.
    
    This change enhances maintainability and allows for easier
    updates to the movie data in the future.

--- watchingList/src/components/List.jsx
+++ watchingList/src/components/List.jsx
@@ -1,52 +1,11 @@
 import ListItem from "./ListItem";
 import {useState} from 'react';
+import {initialShows} from '../data/shows.js'
 
 export default function List(){
-    const [items, setItems] = useState([
-        {
-            id: Date.now().toString()+Math.random(),
-            date: "2025-06-27",
-            title: "Squid game",
-            desc: "Squid game season 3",
-            cate: "Thriler"
-        },
-        {
-            id: Date.now().toString()+Math.random(),
-            date: "2025-07-10",
-            title: "Dr Stone",
-            desc: "the rest of Dr.stone season 4",
-            cate: "Anime, Adventure"
-        },
-        {
-            id: Date.now().toString()+Math.random(),
-            date: "2025-07-19",
-            title: "Kaiju 8",
-            desc: "Kaiju 8 season 2",
-            cate: "Anime, Shonen"
-        },
-        {
-            id: Date.now().toString()+Math.random(),
-            date: "2025-07-14",
-            title: "Dan da dan",
-            desc: "Dan da dan season 2",
-            cate: "Anime, Shonen"
-        },
-        {
-            id: Date.now().toString()+Math.random(),
-            date: "2025-10-01",
-            title: "My hero academia",
-            desc: "Boku no Hero Academia season 8",
-            cate: "Anime, Shonen"
-        },
-        {
-            id: Date.now().toString()+Math.random(),
-            date: "2025-09-01",
-            title: "Alice in borderland",
-            desc: "Alice in borderland season 3",
-            cate: "Thriler"
-        }
-    ]);
+    const [items, setItems] = useState(initialShows);
 
+    console.log(items)
     const apprevMonth=(month)=>{
         const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
         return months[month];
@@ -85,7 +44,6 @@ export default function List(){
                 const day = date.getDate();
                 const month = date.getMonth();
                 const year = date.getFullYear();
-                console.log(apprevMonth(month));
                 return (
                 <ListItem
                     key={item.id}
