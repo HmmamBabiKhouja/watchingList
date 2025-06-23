2fdb4a1bf1b658589b620679300088a63f727020
Hmmam <bhmmam@gmail.com>
2025-06-23 12:08:50 +0300

    feat: implement dark mode toggle and update styles for theme support

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,7 +1,20 @@
-const DarkMode =()=>{
+import {useState, useEffect} from "react"
+import '../index.css'
+
+export default function DarkMode(){
+
+    const [isDark, setTheme] = useState(()=>{
+        return localStorage.getItem("theme")==="dark";
+    });
+
+    useEffect(()=>{
+        document.body.className = isDark? "dark" : "light";
+        localStorage.setItem("theme", isDark ? "dark" : "light")
+    },[isDark]);
+
     return(
         <div className="dark-mode">
-            <button className="btn btn-header" aria-label="dark-mode">
+            <button className="btn btn-header" onClick={()=>setTheme(prev => !prev)} aria-label="dark-mode">
                 <span className="btn-shape">&#9681;</span>
             </button>
         </div>
@@ -9,4 +22,3 @@ const DarkMode =()=>{
 }
 
 
-export default DarkMode
